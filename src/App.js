7c1ead1207384c345cb8e8ffdcbebb74fe1434d6
elCristo6 <cgonzalezsuta@gmail.com>
2023-02-10 15:48:08 -0500

    V1.3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import data from "./data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="grid-container">
+    <nav>
+        <a className = "estiloNav" href="/">¡¡ESTA PAGINA ESTA EN MANTENIMIENTO!!</a>
+    </nav>
+    
+    <header className = "row">
+        
+        <div>
+            <a className = "estiloLogo" href="/">UDElectronics.net</a>
+        </div>
+        <div>
+            <a href="/cart">Ingresa</a>
+            <a href="/signin">Carrito</a>
+        </div>
+    </header>
+    
+    <main>
+        <div>
+            <div className="row center">
+                {
+                     data.products.map(product =>(
+                        <div key={product._id} className="card">
+                            <a href = {`/product/${product._id}`}>
+                            <img className="medium" src={product.image} alt={product.name}/>
+                            </a>
+
+                            <div className="card-body">
+                                <a href = {`/product/${product._id}`} >
+                                    <h2>{product.name}</h2>
+                                </a>
+                                <div className="rating">
+                                    <span><i className="fa fa fa-star"></i></span>
+                                    <span><i className="fa fa fa-star"></i></span>
+                                    <span><i className="fa fa fa-star"></i></span>
+                                    <span><i className="fa fa fa-star"></i></span>
+                                    <span><i className="fa fa fa-star"></i></span>
+                                </div>
+                                <div className="price">  ${product.price}</div>
+                            </div>
+                    </div>
+                    )) }  
+            </div>
+        </div>
+    </main>
+
+    <footer  className="row center" >
+        UD Electronics©2022 CEL: 3208576038-3213213756
+        KRA 9 #19-30 local 202 BY Christian Gonzalez
+    </footer>
+</div>
+
   );
 }
 
